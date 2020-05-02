a87789b614bf47910d54f5ef52e532629ba90102
jsubhani <jsubhani92@gmail.com>
2020-05-02 18:06:47 -0400

    Redid to vid 84

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, present: true, length: {minimum: 6, maximum: 100}
-    validates :description, present: true, length: {minimum: 10, maximum: 300}
+    validates :title, presence: true, length: {minimum: 6, maximum: 100}
+    validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
-endar
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_022217) do
+ActiveRecord::Schema.define(version: 2020_05_01_020759) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+
   end
 
 end
